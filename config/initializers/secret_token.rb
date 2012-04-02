646ee6347f967c7abf3f6862183fb7dc64e89ff2
Dmitriy <link3r88@gmail.com>
2012-04-03 00:58:44 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Besocial::Application.config.secret_token = '88508111bc6c467b830b5d90c719751178c1a7373e6d01e111553446321658d01acc988774314f3aab96dfda427c84030939782fdab48d0333890555f3705cbd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Besocial::Application.config.session_store :cookie_store, key: '_besocial_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Besocial::Application.config.session_store :active_record_store
