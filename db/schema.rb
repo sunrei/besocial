b5f86333c4e9a4e3f63dd865f3d0dfa010937d91
sunrei <sunrei@user1-rezonans.(none)>
2012-04-03 12:45:42 +0300

    added roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120403092321) do
+ActiveRecord::Schema.define(:version => 20120403092929) do
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -26,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20120403092321) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.integer  "role_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
