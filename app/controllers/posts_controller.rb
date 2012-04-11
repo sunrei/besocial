a266c5610e19b9a9f84ecf14769b0e9986204507
Dmitriy <link3r88@gmail.com>
2012-04-11 06:53:26 +0300

    added ajax submit to new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,16 @@ class PostsController < ApplicationController
         @post.user_id = params[:user_id]
         @post.author_id = current_user.id
         @post.save
-        flash[:notice] = ("Please correct errors: " + @post.errors.full_messages * ',') if @post.errors.any?
-        redirect_to user_path(:id => params[:user_id])
+        flash[:notice] =  @post.errors.any? ? ("Please correct errors: " + @post.errors.full_messages * ',') : "Post was successfully created"
+        respond_to do |format|
+          format.html {
+            redirect_to user_path(:id => params[:user_id])
+          }
+          format.js{
+            @user = User.find(params[:user_id])
+            @wall = @user.wall_entries.paginate(:page => params[:page], :per_page => 15)
+          }
+        end
     end
 
     def destroy
