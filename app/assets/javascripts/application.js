f96b8fa76aeccd94304b00dbe9ede1c6567efb80
Dmitriy <link3r88@gmail.com>
2012-04-11 11:51:52 +0300

    added multiple image upload to new post form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,4 +28,20 @@ $(document).ready(function(){
         }
         return false;
     });
+    $("#new_post .file-wrapper input").live('change',function(){
+        //console.log($(this).val());
+        $('#new_post .file-wrapper').prepend("<input type='file' />");
+        $(this).css("top", "50px");
+        $(this).attr('mydata', $(this).val());
+        $(this).attr('name', 'post[pictures_attributes][][picture]');
+        var fileText = "<li>" + $(this).val() + " <a href='#' class='remove_pending' title='Remove this attachment'>Remove</a></li>";
+        $('#pending_list').prepend(fileText);
+        //<input type="file" name="post[pictures_attributes][0][picture]" id="post_pictures_attributes_0_picture">
+    });
+    $('.remove_pending').live('click', function(){
+        var thisIndex = $('#pending_list li').index($(this).parent());
+        $('#new_post .file-wrapper').children().eq(thisIndex+1).remove();
+        $(this).parent().remove();
+        return false;
+    });
 })
