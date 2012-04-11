166b9db355d783b5f03ed2f6dd5665cee321e871
Dmitriy <link3r88@gmail.com>
2012-04-11 12:31:51 +0300

    refactored comments form, comments create action and added ajax on create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,13 @@ class CommentsController < ApplicationController
     #validation
     @post = Post.find(params[:id])
     @user = User.find(@post.user_id)
-    @comment = Comment.new
-    @comment.comment = params[:comment]
+    @comment = Comment.new(params[:comment])
     @comment.post_id = @post.id
     @comment.user_id = current_user.id
-    if @comment.save
-      redirect_to user_path(@post.user_id)
-    else
-      #redirect
-      render :template => "users/show"
+    @comment.save
+    respond_to do |format|
+      format.html{ redirect_to user_path(@post.user_id) }
+      format.js
     end
   end
 
@@ -20,10 +18,7 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     authorize! :delete, @comment
     @comment.destroy
-
     respond_to do |format|
-      format.json { respond_to_destroy(:ajax) }
-      format.xml  { head :ok }
       format.html { redirect_to user_path(@comment.post.user_id), :notice => "Comment was successfully deleted!" }
     end
   end
